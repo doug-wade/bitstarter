a9942d13d5d3efa9e7915d5c63a4fd21e4decd47
Doug Wade <prekolna@gmail.com>
2013-07-07 14:25:48 -0500

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require('express')
+  , fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var htmlBuffer = fs.readFileSync('./index.html');
+  var htmlString = htmlBuffer.toString('utf-8', 0);
+  response.send(htmlString);
 });
 
 var port = process.env.PORT || 5000;
